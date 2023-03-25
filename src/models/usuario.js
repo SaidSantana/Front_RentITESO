3ddb40a2acfe4c9deaf8e524c0a4ecf6c25f28ef
luisEd10 <luis.bruno@iteso.mx>
2023-03-24 18:24:03 -0600

    index actualizado

---
+++ src/models/usuario.js
@@ -0,0 +1,14 @@
+const {Schema, model} = require('mongoose');
+
+const now = new Date();
+
+const usuarioSchema = new Schema({
+    nombre: {type: String},
+    apellido: {type: String},
+    email: {type: String},
+    rol: {type: String},
+    image: {type: String},
+    date: {type: Date, default: now}
+});
+
+module.exports = model('usuarios',usuarioSchema);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const userRouter = require('./usuarios');
 const equipoRoutes = require('./equipos');
 
 router.use('', express.json());
+
 router.use('/equipos', equipoRoutes);
+router.use('/users',userRouter);
 
 module.exports = router;
