eb44d7a56d766e355880babf8c8c9fab1240b948
Said Santana <said.santana@iteso.mx>
2023-03-26 23:13:24 -0600

    CRUD de espacios

---
+++ src/controllers/espacios.js
@@ -0,0 +1,61 @@
+const Espacio = require('./../models/espacio');
+
+class ControladorEspacios{
+    static listar(req, res){
+        Espacio.find({}, null, { collection: 'espacios' }).lean() 
+            .then(espacios => {
+                res.send(espacios);
+            })
+            .catch(err => {
+                res.sendStatus(400);
+            })
+    }
+
+    static consultarId(req, res){
+        Espacio.findById(req.params.id).lean()
+            .then(espacio => {
+                res.send(espacio);
+            })
+            .catch(err => {
+                res.sendStatus(400);
+            })
+    }
+    
+    static postEspacio(req, res){
+        console.log(req.body);
+        const espacio = new Espacio({ 
+            nombre: req.body.nombre,
+            status: req.body.status,
+            descripcion: req.body.descripcion
+        });
+        espacio.save()
+            .then(espacioGuardado => {
+                res.send(espacioGuardado);
+            })
+            .catch(err => {
+                res.sendStatus(400);
+            });
+    }
+
+    static putEspacio(req, res){
+        Espacio.findByIdAndUpdate(req.params.id, req.body).lean()
+            .then(espacio => {
+                res.send(espacio);
+            })
+            .catch(err => {
+                res.sendStatus(400);
+            })
+    }
+
+    static deleteEspacio(req, res){
+        Espacio.findByIdAndDelete(req.params.id)
+            .then(espacio => {
+                res.send(espacio);
+            })
+            .catch(err => {
+                res.sendStatus(400);
+            })
+    }
+}
+
+module.exports = ControladorEspacios;
