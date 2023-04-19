dd131b6e9cecb597e1318e21ab26d2809bf59aac
Said Santana <said.santana@iteso.mx>
2023-04-18 18:40:55 -0600

    Se agregaron los diferentes modulos para el uso de Angular Material, la carpeta de Modules, el layout de tabs, la carpeta de pages con las pages de equipo, espacios, historia, not-found y registro

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -6,17 +6,32 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabsComponent } from './layouts/tabs/tabs.component';
+import { EquipoComponent } from './pages/equipo/equipo.component';
+import { EspaciosComponent } from './pages/espacios/espacios.component';
+import { HistoriaComponent } from './pages/historia/historia.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { MaterialModule } from './modules/material/material.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TabsComponent,
+    EquipoComponent,
+    EspaciosComponent,
+    HistoriaComponent,
+    NotFoundComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
