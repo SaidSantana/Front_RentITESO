9ce5ab9c897c8ac496336d97a226585f9d09828d
Said Santana <said.santana@iteso.mx>
2023-05-12 13:43:45 -0600

    Permisos para Admin para borrar, editar y crear

--- FrontEnd/src/app/pages/espacios/espacios.component.ts
+++ FrontEnd/src/app/pages/espacios/espacios.component.ts
@@ -2,13 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Espacio } from 'src/app/shared/Interface/espacio';
 import { EspaciosService } from 'src/app/shared/services/espacios.service';
+import { User } from 'src/app/shared/Interface/user';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { UsersService } from 'src/app/shared/services/users.service';
+
+import jwtDecode from 'jwt-decode';
 
 @Component({
   selector: 'app-espacios',
   templateUrl: './espacios.component.html',
   styleUrls: ['./espacios.component.scss']
 })
-export class EspaciosComponent implements OnInit{
+export class EspaciosComponent implements OnInit {
   espacios: any[] = [];
   selectedEspacio: any;
   selectedEspacioId: any;
@@ -22,18 +27,29 @@ export class EspaciosComponent implements OnInit{
     status: '',
     image: '',
     stock: 0
-    };
+  };
 
-    constructor(private espaciosService: EspaciosService,private router: Router){ }
+  user!: User;
+  token!: string;
+
+  constructor(private espaciosService: EspaciosService, private router: Router, private authService: AuthService, private usersService: UsersService) { }
 
   ngOnInit(): void {
-      this.espaciosService.getEspacios().subscribe(espacios => {
-        this.espacios = espacios;
-        console.log(espacios);
-      });
+    this.espaciosService.getEspacios().subscribe(espacios => {
+      this.espacios = espacios;
+      console.log(espacios);
+    });
+    this.token = this.authService.getToken() as string;
+    this.bringUser(this.token);
   }
-
-  eliminarEspacio(espacioId: string){
+  bringUser(token: string) {
+    const decodedToken: any = jwtDecode(token);
+    this.usersService.getUser(decodedToken._id).subscribe((user: User) => {
+      this.user = user;
+    });
+    //console.log(this.user.apellido)
+  }
+  eliminarEspacio(espacioId: string) {
     this.espaciosService.deleteEspacios(espacioId).subscribe(() => {
       this.espacios = this.espacios.filter(espacio => espacio._id !== espacioId);
       location.reload();
