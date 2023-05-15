7a3cd790391119c288e5a561c5f43b66f485054c
SaidSantana <73917094+SaidSantana@users.noreply.github.com>
2023-05-15 10:32:31 -0600

    Delete API directory

--- API/src/models/token.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model} = require('mongoose');
-
-const tokenSchema = new Schema({
-    token: {type: String},
-    userId: {type: String}
-});
-
-
-module.exports = model('sesiones', tokenSchema);
