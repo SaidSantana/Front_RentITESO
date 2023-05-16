6a73ae1456a4d0a2c6adf0091b26c3c5bc2982f5
SaidSantana <73917094+SaidSantana@users.noreply.github.com>
2023-05-15 20:01:41 -0600

    Delete src directory

--- src/routes/login.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const usersController = require('./../controllers/usuarios');
-
-router.post('/login', usersController.logIn);
-router.post('/login/google', usersController.loginGoogle);
-router.post('/registro',usersController.registro);
-
-
-module.exports = router;
